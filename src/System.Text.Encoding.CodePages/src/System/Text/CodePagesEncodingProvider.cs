d045c1274e5b49d566d94b3ee82764a0a35cf28b
yinyue200 <yinyue200@outlook.com>
2019-07-13 23:14:04 +0800

    update codes

--- src/System.Text.Encoding.CodePages/src/System/Text/CodePagesEncodingProvider.cs
+++ src/System.Text.Encoding.CodePages/src/System/Text/CodePagesEncodingProvider.cs
@@ -2,11 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Diagnostics.Contracts;
 using System.Collections.Generic;
 using System.Threading;
 
-namespace Yinyue200.ClrSystem.Text.Encoding.CodePages
+namespace System.Text
 {
     public sealed partial class CodePagesEncodingProvider : EncodingProvider
     {
